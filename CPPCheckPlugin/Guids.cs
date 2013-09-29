399736391c87f846ccdbee33971ec6837d1b79fa
VioletGiraffe <alexxxx89@ya.ru>
2013-09-30 00:09:10 +0300

    Initial commit

---
+++ CPPCheckPlugin/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace VSPackage.CPPCheckPlugin
+{
+    static class GuidList
+    {
+        public const string guidCPPCheckPluginPkgString = "127d8bd3-8cd7-491a-9a63-9b4e89118da9";
+        public const string guidCPPCheckPluginCmdSetString = "7fcb87ef-4f0c-4713-8217-5bef43dc0de4";
+        public const string guidToolWindowPersistanceString = "7cc7c6f9-a686-4108-abf5-fab92cd024bc";
+
+        public static readonly Guid guidCPPCheckPluginCmdSet = new Guid(guidCPPCheckPluginCmdSetString);
+    };
+}
---
+++ CPPCheckPlugin/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace VSPackage.CPPCheckPlugin
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidSettings =        0x100;
+        public const uint cmdidMyTool =    0x101;
+
+    };
+}
