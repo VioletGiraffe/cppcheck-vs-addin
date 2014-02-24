c72569e52ca3e8ebcacb506fce7c74467e8c5e09
VioletGiraffe <alexxxx89@ya.ru>
2014-02-24 09:56:55 +0200

    DebugTracer now prints exception in a message so that users can report errors

--- CPPCheckPlugin/DebugTracer.cs
+++ CPPCheckPlugin/DebugTracer.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace VSPackage.CPPCheckPlugin
 {
 	class DebugTracer
 	{
-		[Conditional("DEBUG")]
+		// [Conditional("DEBUG")]
 		public static void Trace(Exception ex)
 		{
-			Debug.WriteLine("Exception occurred in cppcheck add-in: " + ex.Message);
+			Debug.WriteLine("Exception occurred in cppcheck add-in: " + ex.ToString());
+			MessageBox.Show("Exception occurred in cppcheck add-in", ex.ToString());
 		}
 	}
 }
