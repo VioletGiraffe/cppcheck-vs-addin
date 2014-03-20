212ed1605d361030cfcf10e31fe28e6eb2324688
VioletGiraffe <alexxxx89@ya.ru>
2014-03-20 10:16:42 +0200

    Minor Qt excluded path fix, version number updated in the InstalledProductRegistration package attribute (which is what's displayed in Help -> About).

--- CPPCheckPlugin/CPPCheckPluginPackage.cs
+++ CPPCheckPlugin/CPPCheckPluginPackage.cs
@@ -18,7 +18,7 @@ namespace VSPackage.CPPCheckPlugin
 	[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
 	// This attribute is used to register the information needed to show this package
 	// in the Help/About dialog of Visual Studio.
-	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+	[InstalledProductRegistration("#110", "#112", "1.1.0", IconResourceID = 400)]
 	// This attribute is needed to let the shell know that this package exposes some menus.
 	[ProvideMenuResource("Menus.ctmenu", 1)]
 	[ProvideToolWindow(typeof(MainToolWindow), Style=VsDockStyle.Tabbed, Window=Microsoft.VisualStudio.Shell.Interop.ToolWindowGuids.Outputwindow, MultiInstances=false, Transient=false)]
@@ -40,7 +40,7 @@ namespace VSPackage.CPPCheckPlugin
 				suppressionsInfo.SkippedIncludesMask.Add(".*Microsoft SDKs.*");
 				suppressionsInfo.SkippedIncludesMask.Add(".*boost.*");
 				suppressionsInfo.SkippedIncludesMask.Add(@".*\\include\\ActiveQt.*");
-				suppressionsInfo.SkippedIncludesMask.Add(@".*\\include\\Qt.*");
+				suppressionsInfo.SkippedIncludesMask.Add(@".*\\include\\Qt$");
 				suppressionsInfo.SkippedIncludesMask.Add(@".*\\include\\QtCore.*");
 				suppressionsInfo.SkippedIncludesMask.Add(@".*\\include\\QtDeclarative.*");
 				suppressionsInfo.SkippedIncludesMask.Add(@".*\\include\\QtGui.*");
